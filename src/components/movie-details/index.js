843027f97880616ea522b09d48a01ad017100117
upendarch78 <upendarchoppari78@gmail.com>
2024-04-10 16:19:13 +0530

    Updated movie details and cast details

--- src/components/movie-details/index.js
+++ src/components/movie-details/index.js
@@ -4,11 +4,14 @@ import { useParams } from "react-router-dom"
 
 const Movie = () => {
     const [currentMovieDetail, setMovie] = useState()
+    const [MovieCastDetail, setMovieCast] = useState([])
     const { id } = useParams()
 
     useEffect(() => {
         // eslint-disable-next-line
         getData()
+
+        getDatas()
         // eslint-disable-next-line
         window.scrollTo(0,0)
     }, [])
@@ -18,6 +21,14 @@ const Movie = () => {
         .then(res => res.json())
         .then(data => setMovie(data))
     }
+    const getDatas = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=4a9c62f03104a84a45078b8eaf1d8593&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieCast(data.cast))
+
+    }
+
+
 
     return (
         <div className="movie">
@@ -48,6 +59,26 @@ const Movie = () => {
                     </div>
                     
                 </div>
+            </div>
+            <h1 className="casts">CAST</h1>
+            <div className="cast-contaienr">
+
+            
+                {
+                    MovieCastDetail.map(cast => (
+                        <div className="cards">
+                <img className="cards__img" alt="" src={`https://image.tmdb.org/t/p/original${cast ?cast.profile_path : ""}`} />
+                <p className="card__title">{cast.original_name ? cast.original_name :""}</p>
+                <div><p className="card__rating">{cast.character ? cast.character : ""}</p>
+                    </div>
+                    
+            </div>
+
+                        
+                    ))
+                }
+               
+                
             </div>
         </div>
     )
